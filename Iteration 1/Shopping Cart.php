91b5537c508dc1d96e3ee9919a7ee893c26694eb
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-02-14 20:10:56 -0500

    Update Shopping Cart.php

--- Iteration 1/Shopping Cart.php
+++ Iteration 1/Shopping Cart.php
@@ -23,19 +23,45 @@ if ($conn->connect_error) {
 </head>
 
 <?php
-$sql = "SELECT * FROM Item WHERE id in (" . $_POST['items'] . ")";
+$email1 = $_POST['email1'];
+$email2 = $_POST['email2'];
+$pwd1 = $_POST['pwd1'];
+$pwd2 = $_POST['pwd2'];
+$mailAddress = $_POST['deliver'];
+$login = $_POST['login'];
+$valid = false;
+
+if ($login == 'signUp') {
+    $sql = "INSERT INTO User (email, password, mail_address) VALUES ('" . $email1 . "','" . $pwd1 . "','" . $mailAddress . "');";
+    if (mysqli_multi_query($conn, $sql)) {
+        echo "Successfully signed up";
+        $valid = True;
+    } else {
+        echo "Error" . $sql . "<br>" . $conn->error;
+    }
+} elseif ($login == 'signIn') {
+    $sql = "SELECT * FROM User WHERE email='" . $email2 . "' and password='" . $pwd2 . "'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo "Successfully signed in";
+        $valid = True;
+    } else {
+        echo "Incorrect login credentials";
+    }
+}
+
+$sql = "SELECT * FROM Item WHERE id in ('" . $_POST['items'] . "')";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result->num_rows > 0 and $valid == True) {
     echo "<table align='center'><tr><th>ID</th><th>Name</th><th>Price</th></tr>";
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>".$row["id"]."</td><td>".$row["item_name"]."</td><td>".$row["price"]."</td></tr>";
     }
     echo "</table>";
-} else {
+} else if ($valid == True) {
     echo "No items in shopping cart";
 }
-
 $conn->close();
 ?>
 </html>
