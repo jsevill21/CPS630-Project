fbe010c944dfc3b37e605bc5ed72ea0c68528c10
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-02-15 14:02:44 -0500

    Update Process SQL.php

--- Iteration 1/Process SQL.php
+++ Iteration 1/Process SQL.php
@@ -15,45 +15,23 @@ if ($conn->connect_error) {
 <?php
 $action = $_POST['action'];
 $table = $_POST['table'];
-$sql = "";
+$sql = new sql();
 
 if ($action == 'select') {
     if (empty($_POST['condition'])) {
-        $sql = "SELECT " . $_POST['columns'] . " FROM " . $table;
+        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table, $conn);
     } else {
-        $sql = "SELECT " . $_POST['columns'] . " FROM " . $table . " WHERE " . $_POST['condition'];
-    }
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            print_r($row);
-            echo "<br>";
-        }
-    } else {
-        echo "0 results";
+        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table . " WHERE " . $_POST['condition'], $conn);
     }
 } elseif ($action == 'insert') {
-    $sql = "INSERT INTO " . $table . " (" . $_POST['columns']  . ") VALUES (" . $_POST['values'] . ")";
-    
-    if (mysqli_multi_query($conn, $sql)) {
-        echo "Record created successfully";
-    } else {
-        echo "Error" .$sql. "<br>" .$conn->error;
-    }
+    $query = "INSERT INTO " . $table . " (" . $_POST['columns']  . ") VALUES (" . $_POST['values'] . ")";
+    $sql->IDU($query, "Record created sucessfully", "Error creating record", $conn);
 } elseif ($action == 'delete') {
-    $sql = "DELETE FROM " . $table . " WHERE " . $_POST['condition'];
-    if (mysqli_multi_query($conn, $sql)) {
-        echo "Record deleted successfully";
-    } else {
-        echo "Error deleting record: " .$sql. "<br>" .$conn->error;
-    }
+    $query = "DELETE FROM " . $table . " WHERE " . $_POST['condition'];
+    $sql->IDU($query, "Record(s) deleted successfully", "Error deleting record(s)", $conn);
 } elseif ($action == 'update') {
-    $sql = "UPDATE " . $table . " SET " . $_POST['column'] . "=" . $_POST['newValue'] . " WHERE " . $_POST['condition'];
-    if (mysqli_multi_query($conn, $sql)) {
-        echo "Record updated successfully";
-    } else {
-        echo "Error updating record(s): " .$sql. "<br>" .$conn->error;
-    }
+    $query = "UPDATE " . $table . " SET " . $_POST['column'] . "=" . $_POST['newValue'] . " WHERE " . $_POST['condition'];
+    $sql->IDU($query, "Record updated successfully", "Error updating record", $conn);
 }
 $conn->close();
 ?>
