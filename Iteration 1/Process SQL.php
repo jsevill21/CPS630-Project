971382a39284119de3730ba912797229738c52e1
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-02-16 17:48:19 -0500

    Update Process SQL.php

--- Iteration 1/Process SQL.php
+++ Iteration 1/Process SQL.php
@@ -7,19 +7,19 @@ $sql = new sql();
 
 if ($action == 'select') {
     if (empty($_POST['condition'])) {
-        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table, $conn);
+        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table);
     } else {
-        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table . " WHERE " . $_POST['condition'], $conn);
+        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table . " WHERE " . $_POST['condition']);
     }
 } elseif ($action == 'insert') {
     $query = "INSERT INTO " . $table . " (" . $_POST['columns']  . ") VALUES (" . $_POST['values'] . ")";
-    $sql->IDU($query, "Record created sucessfully", "Error creating record", $conn);
+    $sql->IDU($query, "Record created sucessfully", "Error creating record");
 } elseif ($action == 'delete') {
     $query = "DELETE FROM " . $table . " WHERE " . $_POST['condition'];
-    $sql->IDU($query, "Record(s) deleted successfully", "Error deleting record(s)", $conn);
+    $sql->IDU($query, "Record(s) deleted successfully", "Error deleting record(s)");
 } elseif ($action == 'update') {
     $query = "UPDATE " . $table . " SET " . $_POST['column'] . "=" . $_POST['newValue'] . " WHERE " . $_POST['condition'];
-    $sql->IDU($query, "Record updated successfully", "Error updating record", $conn);
+    $sql->IDU($query, "Record updated successfully", "Error updating record");
 }
 $conn->close();
 ?>
