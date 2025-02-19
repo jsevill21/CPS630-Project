dfe960c3aeaa7dfe4e8195170d5551cb88820f6f
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-02-19 15:32:18 -0500

    Update Admin.php

--- Iteration 1 and 2/Admin.php
+++ Iteration 1 and 2/Admin.php
@@ -1,25 +1,37 @@
 <?php include ('PHP Class.php'); ?>
 
-<?php
-$action = $_POST['action'];
-$table = $_POST['table'];
-$sql = new sql($conn);
+<!DOCTYPE html>
+<html lang="eng">
+    
+<head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
-if ($action == 'select') {
-    if (empty($_POST['condition'])) {
-        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table);
-    } else {
-        $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table . " WHERE " . $_POST['condition']);
+<body>
+    <?php
+    $action = $_POST['action'];
+    $table = $_POST['table'];
+    $sql = new sql($conn);
+    
+    if ($action == 'select') {
+        if (empty($_POST['condition'])) {
+            $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table);
+        } else {
+            $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table . " WHERE " . $_POST['condition']);
+        }
+    } elseif ($action == 'insert') {
+        $query = "INSERT INTO " . $table . " (" . $_POST['columns']  . ") VALUES (" . $_POST['values'] . ")";
+        $sql->IDU($query, "Record created sucessfully", "Error creating record");
+    } elseif ($action == 'delete') {
+        $query = "DELETE FROM " . $table . " WHERE " . $_POST['condition'];
+        $sql->IDU($query, "Record(s) deleted successfully", "Error deleting record(s)");
+    } elseif ($action == 'update') {
+        $query = "UPDATE " . $table . " SET " . $_POST['column'] . "=" . $_POST['newValue'] . " WHERE " . $_POST['condition'];
+        $sql->IDU($query, "Record updated successfully", "Error updating record");
     }
-} elseif ($action == 'insert') {
-    $query = "INSERT INTO " . $table . " (" . $_POST['columns']  . ") VALUES (" . $_POST['values'] . ")";
-    $sql->IDU($query, "Record created sucessfully", "Error creating record");
-} elseif ($action == 'delete') {
-    $query = "DELETE FROM " . $table . " WHERE " . $_POST['condition'];
-    $sql->IDU($query, "Record(s) deleted successfully", "Error deleting record(s)");
-} elseif ($action == 'update') {
-    $query = "UPDATE " . $table . " SET " . $_POST['column'] . "=" . $_POST['newValue'] . " WHERE " . $_POST['condition'];
-    $sql->IDU($query, "Record updated successfully", "Error updating record");
-}
-$conn->close();
-?>
+    $conn->close();
+    ?>
+</body>
+</html>
