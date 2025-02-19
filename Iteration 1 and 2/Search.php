b11dfd65a947be4e2a0585eb0b81014cf40467cc
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-02-19 15:31:14 -0500

    Update Search.php

--- Iteration 1 and 2/Search.php
+++ Iteration 1 and 2/Search.php
@@ -6,16 +6,14 @@
 <head>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search</title>
-    <style>
-        body {text-align: center;}
-    </style>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
-<body>
+<body style="text-align: center;">
     <h1>Order Search</h1>
     <?php
     $order_id = $_POST['order_id'];
-    echo "<table align='center'><tr><th>Email</th><th>Order ID</th></tr>";
+    echo "<table style='margin: auto auto';><tr><th>Email</th><th>Order ID</th></tr>";
 
     $query = "SELECT * FROM Orders WHERE order_id=" . $order_id;
     $sql = new sql($conn);
