64fc1bb9251309a611253c82ce1ebe9868e47169
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-02-23 16:16:44 -0500

    Update Admin.php

--- Iteration 1 and 2/Admin.php
+++ Iteration 1 and 2/Admin.php
@@ -20,16 +20,16 @@
         if (empty($_POST['condition'])) {
             $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table);
         } else {
-            $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table . " WHERE " . $_POST['condition']);
+            $sql->print_at_once("SELECT " . $_POST['columns'] . " FROM " . $table . " " . $_POST['condition']);
         }
     } elseif ($action == 'insert') {
         $query = "INSERT INTO " . $table . " (" . $_POST['columns']  . ") VALUES (" . $_POST['values'] . ")";
         $sql->IDU($query, "Record created sucessfully", "Error creating record");
     } elseif ($action == 'delete') {
-        $query = "DELETE FROM " . $table . " WHERE " . $_POST['condition'];
+        $query = "DELETE FROM " . $table . " " . $_POST['condition'];
         $sql->IDU($query, "Record(s) deleted successfully", "Error deleting record(s)");
     } elseif ($action == 'update') {
-        $query = "UPDATE " . $table . " SET " . $_POST['column'] . "=" . $_POST['newValue'] . " WHERE " . $_POST['condition'];
+        $query = "UPDATE " . $table . " SET " . $_POST['column'] . "=" . $_POST['newValue'] . " " . $_POST['condition'];
         $sql->IDU($query, "Record updated successfully", "Error updating record");
     }
     $conn->close();
