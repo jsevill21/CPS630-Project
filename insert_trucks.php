8b856b25f347ddd43d4991147c012eca20aae696
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-03-02 17:47:42 -0500

    Update insert_trucks.php

--- insert_trucks.php
+++ insert_trucks.php
@@ -13,7 +13,8 @@ if ($conn->connect_error) {
 ?>
 
 <?php
-$sql = "INSERT INTO Truck (store_id) VALUES (1);";
+$sql = "INSERT INTO Truck (store_id) VALUES (2);";
+$sql .= "INSERT INTO Truck (store_id) VALUES (1);";
 
 if ($conn->multi_query($sql)) {
     echo "Records inserted successfully";
