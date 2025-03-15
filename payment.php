a651f6ba945691f9fde51d8c6528029b768c7789
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-03-15 19:12:44 -0400

    Add files via upload

---
+++ payment.php
@@ -0,0 +1,34 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "osp";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+<?php
+$sql = "CREATE TABLE Payment (
+payment_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(50) NOT NULL,
+payment_option VARCHAR(50) NOT NULL,
+card_name VARCHAR(50),
+card_number INT(6),
+paypal_email VARCHAR(50),
+paypal_pwd VARCHAR(50),
+FOREIGN KEY (email) REFERENCES User(email)
+);";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table Payment created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?> 
