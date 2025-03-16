9a60884e56eb471f90245c533709d7013935bac7
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-03-16 16:26:24 -0400

    Update orders.php

--- orders.php
+++ orders.php
@@ -15,10 +15,8 @@ if ($conn->connect_error) {
 <?php
 $sql = "CREATE TABLE Orders (
 order_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-email VARCHAR(50) NOT NULL,
 trip_id INT(6) UNSIGNED NOT NULL,
 payment_id INT(6) UNSIGNED NOT NULL,
-FOREIGN KEY (email) REFERENCES User(email),
 FOREIGN KEY (trip_id) REFERENCES Trip(trip_id),
 FOREIGN KEY (payment_id) REFERENCES Payment(payment_id)
 );";
