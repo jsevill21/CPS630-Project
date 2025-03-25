c1526b970b9cf2080bcdb24e9a6465a704f58083
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-03-25 18:06:54 -0400

    Add files via upload

---
+++ Iteration 3 and 4/insert_reviews.php
@@ -0,0 +1,22 @@
+<?php include ('connect.php'); ?>
+<?php include ('PHP Class.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Review Submitted</title>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body style="text-align: center;">
+<?php
+$item_id = $_POST['item_id'];
+$rn = $_POST['rn'];
+$review = $_POST['review'];
+$sql = new sql($conn);
+$query = "INSERT INTO Reviews (item_id, rn, review) VALUES (" . $item_id . "," . $rn . ",'" . $review . "')";
+$sql->IDU($query, "Review Submitted", "Error submitting review");
+$conn->close();
+?>
+</body>
+</html>
