448bc43de2f08a77615319d28590084ccafeb700
jsevill21 <121982255+jsevill21@users.noreply.github.com>
2025-03-25 18:02:57 -0400

    Add files via upload

---
+++ Iteration 3 and 4/iphone.php
@@ -0,0 +1,10 @@
+<?php include ('connect.php'); ?>
+<?php include ('PHP Class.php'); ?>
+
+<?php
+$sql = new sql($conn);
+$query = "SELECT * FROM Reviews WHERE item_id=5";
+echo "<table style='margin: auto; text-align: center;'><tr><th>Review</th><th>Ranking Number</th></tr>";
+$sql->print_table($query, ['review', 'rn']);
+echo "</table>";
+?>
